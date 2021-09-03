e26eefb1a585e0cfaaf085fb85e33d5125a56ca0
hugo-maker <hibiki.for.business1@gmail.com>
2021-09-03 16:18:12 +0900

    Added today's activities

---
+++ abc_d/abc064_insertion.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int N;
+  string S;
+  cin >> N >> S;
+
+  int need_left = 0;
+  int pointer = 0;
+  for (int i = 0; i < S.size(); ++i)
+  {
+    if (S[i] == '(')
+    {
+      ++pointer;
+    }
+    else
+    {
+      if (pointer == 0)
+      {
+        ++need_left;
+      }
+      else
+      {
+        --pointer;
+      }
+    }
+  }
+
+  int need_right = pointer;
+  string res = "";
+  for (int i = 0; i < need_left; ++i)
+  {
+    res += '(';
+  }
+  res += S;
+  for (int i = 0; i < need_right; ++i)
+  {
+    res += ')';
+  }
+  cout << res << endl;
+  return 0;
+}
