00f64f742f5698b9f450943197c918bb574890d8
hugo-maker <hibiki.for.business1@gmail.com>
2021-12-26 15:59:59 +0900

    Added today's activities

---
+++ abc_b/abc233_a_reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int L, R;
+  string S;
+  cin >> L >> R >> S;
+  reverse(S.begin() + L - 1, S.begin() + R);
+  cout << S << endl;
+  return 0;
+}
